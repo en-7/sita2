8f592cadb06a50b5b70c71474e0ba0a3cdbeb8a9
Assyifa Raudina <assyifa.raudina@ui.ac.id>
2023-12-03 01:56:04 +0700

    reverted connection

--- sita/sita/src/main/java/protensi/sita/controller/UGBController.java
+++ sita/sita/src/main/java/protensi/sita/controller/UGBController.java
@@ -30,7 +30,10 @@ import protensi.sita.service.UgbServiceImpl;
 import protensi.sita.service.BaseService;
 import protensi.sita.service.TimelineServiceImpl;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -66,6 +69,12 @@ public class UGBController {
 
         TimelineModel tl = tlService.checkDate();
         LocalDate nowDate = LocalDate.now();
+        Instant now = Instant.now();
+        ZonedDateTime nowJakarta = now.atZone(ZoneId.of("Asia/Jakarta")); 
+  
+        LocalDate nowTimezonedDate = nowJakarta.toLocalDate();
+
+        System.out.println("nowTimeZonedDate: "+ nowTimezonedDate);
 
         if (retrievedUgb != null) {
             String idUgb = retrievedUgb.getIdUgb().toString();
